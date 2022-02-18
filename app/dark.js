300e3cbea873b27e9a1a1d10665821ee10ad48ff
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-02-18 09:43:39 +0300

    added dark mode

---
+++ app/dark.js
@@ -0,0 +1,23 @@
+const Switch = document.getElementById("switch")
+const darkBody = document.querySelectorAll(".dark-mode")
+Switch.addEventListener("change", darkmode)
+
+function darkmode(e) {
+    let dark = e.currentTarget.checked
+
+
+    if (dark === true) {
+        darkBody.forEach(function(body) {
+            console.log(body)
+            body.classList.add("dark")
+
+        });
+
+
+    } else if (dark === false) {
+        darkBody.forEach(function(body) {
+            body.classList.remove("dark")
+
+        });
+    }
+}
