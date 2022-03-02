aa63d3d4dd0b5f15cf66b756da055e48bba09f95
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-03-02 10:08:06 +0300

    edit

--- app/js/dark.js
+++ app/js/dark.js
@@ -1,17 +1,24 @@
 const Switch = document.getElementById("switch")
 const darkBody = document.querySelectorAll(".dark-mode,.form-control")
-Switch.addEventListener("change", darkmode)
-
+Switch.addEventListener("change", darkmode )
+darkmode
 function darkmode(e) {
-    let dark = e.currentTarget.checked
-    if (dark === true) {
+  let dark =e.currentTarget.checked
+  darkStorage(dark)
+    if (theme === true) {
         darkBody.forEach(function(body) {
             console.log(body)
             body.classList.add("dark")
         });
-    } else if (dark === false) {
+    } else if (theme === false) {
         darkBody.forEach(function(body) {
             body.classList.remove("dark")
         });
     }
+}
+function darkStorage(dark){
+    let theme;
+    localStorage.setItem("dark-theme",dark)
+    theme = JSON.parse(localStorage.getItem("dark-theme"))
+    return !theme
 }
